c28aa458ba22d618ad998a4fb4b865a34763e512
Zenith <96979639+appyjuss@users.noreply.github.com>
2024-11-09 12:39:20 -0600

    fecth project

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,12 +1,13 @@
 // routes/projectRoutes.js
 const express = require('express');
-const { getProjects, createProject } = require('../controllers/projectController'); // Import the controller
+const { fetchProjectsByUser, createProject } = require('../controllers/projectController'); // Import the controller
 
 const router = express.Router();
 
 // Existing route for listing all projects
-router.get('/list', getProjects);
+router.get('/list', fetchProjectsByUser);
 router.post('/create', createProject);
+
 
 // // New route to fetch projects by userId
 // router.get('/user/:userId', getProjectsByUserId);
